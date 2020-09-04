9d59d6317bc256520d124c5fc88d394e1bd71ef4
Makoto87 <makosinhori@gmail.com>
2020-09-04 15:36:55 +0900

    made answer

--- all.php
+++ all.php
@@ -1,129 +1,82 @@
 <?php
+// square(正方形)、rectangle(長方形)、kite(凧)、rhombus(ひし形)、parallelogram(平行四辺形)、trapezoid(台形)
 
-// ここから書いてください
-class Point{
-    public $x;
-    public $y;
+// 4点A,B,C,Dを受け取って、図形の名前を返す、getShapeTypeという関数を作成してください。
+function getShapeType($pointA,$pointB,$pointC,$pointD) {
+    $points = [$pointA,$pointB,$pointC,$pointD];
 
-    function __construct($x, $y){
-        $this->x = $x;
-        $this->y = $y;
-    }
-}
-
-class Line{
-    public Point $startPoint;
-    public Point $endPoint;
-
-    function __construct(Point $startPoint, Point $endPoint){
-        $this->startPoint = $startPoint;
-        $this->endPoint = $endPoint;
-    }
-}
-
-class QuadrilateralShape {
-    public Line $lineA;
-    public Line $lineB;
-    public Line $lineC;
-    public Line $lineD;
-
-    // 各辺の長さ
-    public $lengthA;
-    public $lengthB;
-    public $lengthC;
-    public $lengthD;
-    public $lengthAC;
-
-    function __construct(Line $lineA, Line $lineB, Line $lineC, Line $lineD) {
-        $this->lineA = $lineA;
-        $this->lineB = $lineB;
-        $this->lineC = $lineC;
-        $this->lineD = $lineD;
-    }
-
-    // 形が何かを返す
-    // function getShapeType() {
-        
-    // }
-
-    // 四辺の長さを返す
-    function getPerimeter() {
-        $length = 0;
-        // lineAの辺の長さを足す。メンバ変数に格納する。
-        $this->lengthA = sqrt(($this->lineA->endPoint->x - $this->lineA->startPoint->x) ** 2 + ($this->lineA->endPoint->y - $this->lineA->startPoint->y) ** 2);
-        $length += $this->lengthA;
-        
-        // lineB
-        $this->lengthB = sqrt(($this->lineB->endPoint->x - $this->lineB->startPoint->x) ** 2 + ($this->lineB->endPoint->y - $this->lineB->startPoint->y) ** 2);
-        $length += $this->lengthB;
-
-        // lineC
-        $this->lengthC = sqrt(($this->lineC->endPoint->x - $this->lineC->startPoint->x) ** 2 + ($this->lineC->endPoint->y - $this->lineC->startPoint->y) ** 2);
-        $length += $this->lengthC;
-
-        // lineD
-        $this->lengthD = sqrt(($this->lineD->endPoint->x - $this->lineD->startPoint->x) ** 2 + ($this->lineD->endPoint->y - $this->lineD->startPoint->y) ** 2);
-        $length += $this->lengthD;
-
-        return $length;
-    }
-
-    // 四角形の面積を求める。ヘロンの公式を使う
-    // 長さを求めた後に処理をしなければ、各辺の長さが格納していないので論理エラーが起こる
-    function getArea() {
-        // 開始の点と3つめの点を結ぶ線
-        $acLineLength = sqrt(($this->lineA->startPoint->x - $this->lineB->endPoint->x) ** 2 + ($this->lineA->startPoint->y - $this->lineB->endPoint->y) ** 2);
-        $this->$lengthAC = $acLineLength;
-        // 開始・2つめ・3つめの点を結んだ面積を求める
-        $sABC = ($this->lengthA + $this->lengthB + $acLineLength) / 2;
-        $halfAreaABC = sqrt($sABC * ($sABC - $this->lengthA) * ($sABC - $this->lengthB) * ($sABC - $acLineLength));
-        // 3つめ・4つめ・終わりの点を結んだ面積を求める
-        $sCDA = ($this->lengthC + $this->lengthD + $acLineLength) / 2;
-        $halfAreaCDA = sqrt($sCDA * ($sCDA - $this->lengthC) * ($sCDA - $this->lengthD) * ($sCDA - $acLineLength));
-
-        return $halfAreaABC + $halfAreaCDA;
-    }
-
-    // 角度を返す。BAD, ABC, ADC, BCDのどれかが入力される
-    function getAngle($angleString) {
-        // 2つめと4つめの点を結んだ線
-        $bdLineLength = sqrt(($this->lineB->startPoint->x - $this->lineC->endPoint->x) ** 2 + ($this->lineB->startPoint->y - $this->linCB->endPoint->y) ** 2);
-        // 角度を求める。コサイン->アークコサイン->角度
-        if ($angleString == "BAD") {
-            $cosine = (($this->lengthA) ** 2 + ($this->lengthD) ** 2 - $bdLineLength ** 2) / (2 * $this->lengthA * $this->lengthD);
-            return rad2deg(acos($cosine));
-        } else if ($angleString == "ABC") {
-            $cosine = (($this->lengthA) ** 2 + ($this->lengthB) ** 2 - $this->$lengthAC ** 2) / (2 * $this->lengthA * $this->lengthB);
-            return rad2deg(acos($cosine));
-        } else if($angleString == "ADC") {
-            $cosine = (($this->lengthD) ** 2 + ($this->lengthC) ** 2 - $this->$lengthAC ** 2) / (2 * $this->lengthD * $this->lengthC);
-            return rad2deg(acos($cosine));
-        } 
-        else {
-            $cosine = (($this->lengthB) ** 2 + ($this->lengthC) ** 2 - $bdLineLength ** 2) / (2 * $this->lengthB * $this->lengthC);
-            return rad2deg(acos($cosine));
+    // 四角形にならない条件 同じ座標の点がある
+    for ($i = 0; $i < count($points) - 1; $i++) {
+        if (array_search($points[$i], array_slice($points,$i+1)) !== false) {
+            return "not rectangle";
         }
-        
     }
+
+    // 四辺の長さを求める
+    $lineAB = sqrt(($pointA[0] - $pointB[0]) ** 2 + ($pointA[1] - $pointB[1]) ** 2);
+    $lineBC = sqrt(($pointB[0] - $pointC[0]) ** 2 + ($pointB[1] - $pointC[1]) ** 2);
+    $lineCD = sqrt(($pointC[0] - $pointD[0]) ** 2 + ($pointC[1] - $pointD[1]) ** 2);
+    $lineDA = sqrt(($pointD[0] - $pointA[0]) ** 2 + ($pointD[1] - $pointA[1]) ** 2);
+
+    // ４つの角度を求める
+    $angleA = angleCalc($pointB[0], $pointB[1], $pointA[0], $pointA[1], $pointD[0], $pointD[1]);
+    $angleB = angleCalc($pointA[0], $pointA[1], $pointB[0], $pointB[1], $pointC[0], $pointC[1]);
+    $angleC = angleCalc($pointB[0], $pointB[1], $pointC[0], $pointC[1], $pointD[0], $pointD[1]);
+    $angleD = angleCalc($pointA[0], $pointA[1], $pointD[0], $pointD[1], $pointC[0], $pointC[1]);
+
+    // 四角形がどの形か求める
+    // 全ての辺が同じ長さの場合
+    if ($lineAB == $lineBC && $lineBC == $lineCD && $lineCD == $lineDA && $lineDA == $lineAB) {
+        // 正方形
+        if ($angleA == 90) {
+            return "square(正方形)";
+        // ひし形
+        } else {
+            return "rhombus(ひし形)";
+        }
+    }
+    // 向かい合ってる辺が同じ長さの場合
+    if ($lineAB == $lineCD && $lineBC == $lineDA) {
+        // 長方形
+        if ($angleA == 90) {
+            return "rectangle(長方形)";
+        // 平行四辺形
+        } else {
+            return "parallelogram(平行四辺形)";
+        }
+    }
+    // 台形
+    if ($angleA + $angleB == 180 || $angleB + $angleC == 180 || $angleC + $angleD == 180 || $angleA + $angleD == 180) {
+        return "trapezoid(台形)";
+    }
+    // 凧
+    if (($lineAB == $lineBC && $lineCD == $lineDA) || ($lineAB == $lineDA && $lineBC == $lineCD)) { return "kite(凧)"; }
+    // その他
+    return "その他";
+    
+    // return $angleA;
 }
 
-// 線を用意する
-$line1 = new Line(new Point(0,0), new Point(2,6));
-$line2 = new Line(new Point(2,6), new Point(0,8));
-$line3 = new Line(new Point(0,8), new Point(-2,6));
-$line4 = new Line(new Point(-2,6), new Point(0,0));
+// 角度を求める関数
+function angleCalc($a1,$a2,$b1,$b2,$c1,$c2){
+        $a1 -= $b1; $a2 -= $b2;
+        $c1 -= $b1; $c2 -= $b2;
+        $cosine = ($a1*$c1 + $a2*$c2) / ((($a1**2+$a2**2)**0.5) * (($c1**2+$c2**2)**0.5));
+        return round((rad2deg(acos($cosine))),1);
+}
 
-// 四角形を作る
-$quadrilateralShape = new QuadrilateralShape($line1,$line2,$line3,$line4);
+// 正方形
+echo getShapeType([0,2],[2,2],[2,4],[0,4]) . PHP_EOL;
+// ひし形
+echo getShapeType([0,0],[5,0],[8,4],[3,4]) . PHP_EOL;
+// 長方形
+echo getShapeType([0,0],[5,0],[5,8],[0,8]) . PHP_EOL;
+// 平行四辺形
+echo getShapeType([0,0],[5,0],[8,8],[3,8]) . PHP_EOL;
+// 台形
+echo getShapeType([-2,0],[5,0],[8,8],[-1,8]) . PHP_EOL;
+// 凧
+echo getShapeType([0,0],[5,3],[0,8],[-5,3]) . PHP_EOL;
+// その他
+echo getShapeType([0,1],[2,3],[3,4],[2,1]) . PHP_EOL;
 
-// 四辺の長さを出力する
-echo $quadrilateralShape->getPerimeter() . PHP_EOL;
-
-// 面積を出力する
-echo $quadrilateralShape->getArea() . PHP_EOL;
-
-// 角度を出力する
-echo $quadrilateralShape->getAngle("BAD").  PHP_EOL;
-echo $quadrilateralShape->getAngle("ABC").  PHP_EOL;
-echo $quadrilateralShape->getAngle("ADC").  PHP_EOL;
-echo $quadrilateralShape->getAngle("BCD").  PHP_EOL;
